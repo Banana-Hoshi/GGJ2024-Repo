b949ccd9cc957a1a43e1b668821c0f0041bcd76a
Lucas Abramczuk <lucas.abramczuk@ontariotechu.net>
2024-01-27 13:53:43 -0500

    prefab change

--- UnityProject/Assets/Lucas/Scripts/Person.cs
+++ UnityProject/Assets/Lucas/Scripts/Person.cs
@@ -7,7 +7,7 @@ public class Person : MonoBehaviour
     private CrewManager crewManager;
     private void Update()
     {
-        if(Vector3.Distance(transform.position, transform.parent.position) > 150f) //?
+        if(Vector3.Distance(transform.position, transform.parent.position) > 150f) //???
         {
             crewManager.KillPerson(gameObject.name);
             Destroy(gameObject, 0.2f); //0.2 so that the crew manager can find and remove it from the list without hitting a null reference
