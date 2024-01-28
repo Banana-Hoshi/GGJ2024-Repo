94229f32c65b75ae0fc8d4976a4f5c59b8dfb745
Jonathan Jay <56273460+Jonathan-Jay@users.noreply.github.com>
2024-01-28 13:03:19 -0500

    started boat and stuff

--- UnityProject/Assets/Scripts/DayNight/LightingManager.cs
+++ UnityProject/Assets/Scripts/DayNight/LightingManager.cs
@@ -60,7 +60,7 @@ public class LightingManager : MonoBehaviour
 
         else
         {
-            Light[] lights = GameObject.FindObjectsOfType<Light>();
+            Light[] lights = FindObjectsByType<Light>(FindObjectsSortMode.None);
             foreach (Light light in lights)
             {
                 if (light.type == LightType.Directional)
